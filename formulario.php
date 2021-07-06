9f76afefabbc06cf34d5746dec0d765def5c061f
Margarita29Sa <86939106+Margarita29Sa@users.noreply.github.com>
2021-07-06 00:24:48 -0500

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+    <meta charset=utf-8>
+    <link rel="stylesheet" type="text/css" href="cssEst/estilosphplocal.css">
+    </head>
+<body class="cp"> 
+<?php
+    $nombre=$_POST['nomest'];
+
+    echo ("¡BIENVENIDO! <br>");
+    echo ("Tu establecimiento $nombre ha sido registrado exitosamente");
+
+?>
+
+</body>
+</html>
